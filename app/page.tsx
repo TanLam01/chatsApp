1a93fd8ccc6eb4bf57c5b0b7d02c6d2455040233
Tất Lâm <tanlam15112001@gmail.com>
2024-05-18 11:41:07 +0700

    Update code

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-                Chat with Anyone, anywhere!
+                Chats App!
               </h1>
 
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
