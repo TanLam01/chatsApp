27770a40a89d237aa0198f44d36b5bd930869027
Tất Lâm <tanlam15112001@gmail.com>
2024-05-22 14:29:48 +0700

    fix css

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -61,7 +61,7 @@ function ChatListRow({ chatId }: { chatId: string }) {
                 <div className="flex p-5 items-center space-x-2">
                     <Skeleton className="h-12 w-12 rounded-full" />
                     <div className="space-y-2 flex-1">
-                        <Skeleton className="h-2 w-full" />
+                        <Skeleton className="h-4 w-full" />
                         <Skeleton className="h-4 w-1/4" />
                     </div>
                 </div>
